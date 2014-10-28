fdbaa0c5d6a361f9cc0e90d45f86543af0e342e8
Ana Lopez <alumno.343806@ies-azarquiel.es>
2014-10-28 21:38:16 +0100

    Punto de partida

---
+++ HolaMundo2/src/Modelo/SAluda.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+public class SAluda {
+
+	public SAluda() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void diHola(String nombre) {
+		System.out.println("Hola: " + nombre + " ¿Qué tal?");
+
+	}
+
+}
---
+++ HolaMundo2/src/controlador/Principal.java
@@ -0,0 +1,17 @@
+package controlador;
+
+import Modelo.SAluda;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		SAluda saludo=new SAluda ();
+		saludo.diHola("Ana");
+
+	}
+
+}
